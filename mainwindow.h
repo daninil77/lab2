787ee7608e88e5dc6d73045304c1f5f2d774263b
daninil77 <danil_game1@mail.ru>
2022-02-24 17:33:58 +0300

    max

---
+++ mainwindow.h
@@ -0,0 +1,29 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+#include <occupation.h>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+public slots:
+    void findOccupation();
+
+private:
+    Ui::MainWindow *ui;
+
+ std::vector<Occupation> zanytiya;
+
+};
+#endif // MAINWINDOW_H
